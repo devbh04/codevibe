9007048cef665e536f41f2ddaa0dca5f3936e4f1
devbh04 <devbhangale.work@gmail.com>
2025-04-19 13:43:48 +0530

    id error solved for deployment

--- app/(root)/code-editor/[id]/page.tsx
+++ app/(root)/code-editor/[id]/page.tsx
@@ -1,6 +1,6 @@
+// app/(root)/code-editor/[id]/page.tsx
 import CodeEditor from "../code-editor";
 
-
-export default function CodeEditorPage({ params }: { params: { id: string } }) {
+export default function CodeEditorPage() {
   return <CodeEditor />;
 }
