9bee975a8a324493068d43a79ecd4eb41355d944
devbh04 <devbhangale.work@gmail.com>
2025-04-20 00:36:35 +0530

    homepage customizations

--- components/shared/appbar.tsx
+++ components/shared/appbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const AppBAr = () => {
   return (
-    <div className='p-4 px-10 bg-slate-800 flex justify-between items-center'>
+    <div className='p-4 px-10 bg-slate-900 flex justify-between items-center'>
       <div className='flex items-center space-x-6'>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-8 w-8 text-purple-500">
           <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
